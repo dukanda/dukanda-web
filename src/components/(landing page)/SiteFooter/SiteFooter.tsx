66ae245d54db7814bc2ffbb589d0207f1a17be43
Mário Salvador <128391174+mariosalvador@users.noreply.github.com>
2025-03-17 17:24:27 +0000

    feat: :sparkles: enhance SiteFooter with new input component and styling updates

--- src/components/(landing page)/SiteFooter/SiteFooter.tsx
+++ src/components/(landing page)/SiteFooter/SiteFooter.tsx
@@ -1,4 +1,6 @@
+import { Input } from "@/components/ui/input";
 import footerData from "@/data/footerData";
+import { MoveUp } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
@@ -29,7 +31,7 @@ const SiteFooter = () => {
                   <p className="text-[15px] text-gray-400 leading-[32px] pt-[29px] pb-[24px] mb-[18px] border-b border-gray-700">
                     {about}
                   </p>
-                  <ul className="list-none">
+                  <ul className="list-none bg-red-500">
                     {icons.map(({ id, icon, content, subHref }) => (
                       <li key={id} className="flex items-center space-x-[20px] mt-2">
                         <div className="flex items-center">
@@ -86,20 +88,20 @@ const SiteFooter = () => {
                   <h3 className="text-[18px] font-bold text-white mb-[25px]">Newsletter</h3>
                   <form onSubmit={handleSubmit} className="pt-[12px]">
                     <div className="relative">
-                      <input
+                      <Input
                         type="email"
                         placeholder="Email"
                         name="email"
                         required
-                        className="w-full h-[67px] bg-gray-800 border-none text-[13px] text-gray-400 font-medium capitalize text-center rounded-lg px-[30px]"
+                        className="w-full h-[67px] bg-white border-none text-[13px] text-gray-800 font-medium capitalize text-start rounded-lg px-[30px]"
                       />
-                      <button type="submit" className="w-full mt-[10px] py-[17px] text-[15px] font-bold uppercase bg-primary text-white rounded-lg transition hover:bg-white hover:text-primary">
+                      <button type="submit" className="w-full mt-[10px] py-[17px] text-[15px] font-bold uppercase bg-orange-500 text-white  rounded-lg transition hover:bg-orange-600">
                         Subscrever
                       </button>
                     </div>
                   </form>
-                  <div className="flex items-center mt-[12px]">
-                    <div className="h-[18px] w-[18px] border border-gray-400 rounded-full flex items-center justify-center">
+                  <div className="flex mt-[12px]">
+                    <div className="h-[18px] w-[18px] mt-2 border border-gray-400 rounded-full flex items-center justify-center">
                       <i className="fa fa-check text-[8px] text-gray-400" />
                     </div>
                     <p className="ml-[10px] text-[15px] text-gray-400 font-medium">
@@ -112,8 +114,8 @@ const SiteFooter = () => {
           </div>
         </Container>
       </div>
-      <div className="relative flex items-center justify-between bg-white rounded-tl-[10px] py-[31px] px-[160px] z-[1]">
-        <div className="flex items-center">
+      <div className="relative flex items-center justify-between bg-white rounded-tl-[10px] py-[20px] px-[160px] z-[1]">
+        <div className="flex items-center gap-2">
           {social.map(({ icon, link }, index) => (
             <a
               key={index}
@@ -125,11 +127,13 @@ const SiteFooter = () => {
           ))}
         </div>
         <p className="text-[15px] text-gray-500 font-medium">
-          &copy; {year}, <a href="#" className="hover:text-primary transition">{author}</a>
+          &copy; {year} - <a href="#" className="hover:text-primary transition">{author}</a>
         </p>
         <div className="absolute top-0 bottom-0 left-0">
-          <a href="#" className="absolute top-0 bottom-0 left-0 w-[122px] flex items-center justify-center bg-primary rounded-tl-[8px] transition hover:bg-gray-800 hover:text-white">
-            <span className="rotate-[-90deg] text-white">↑</span>
+          <a href="#" className="absolute top-0 bottom-0 left-0 w-[122px] flex items-center justify-center bg-orange-500 rounded-tl-[8px] transition hover:bg-orange-700 hover:text-white">
+            <span className=" text-white">
+              <MoveUp size={20} />
+            </span>
           </a>
         </div>
       </div>
